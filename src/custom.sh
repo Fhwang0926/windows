75238f95449018b26eb95982811cb3e76b57f32c
Fhwang <donghwa.dev@gmail.com>
2024-02-07 23:43:21 +0900

    test

--- src/custom.sh
+++ src/custom.sh
@@ -16,7 +16,7 @@ DISK_OPTS="$DISK_OPTS \
 #   -append rootfstype=virtiofs /tmp/virtiofsd.sock rw"
 
 # exute fs
-/usr/libexec/virtiofsd --socket-path=/tmp/virtiofsd.sock --shared-dir /opt --cache auto
+nohup /usr/libexec/virtiofsd --socket-path=/tmp/virtiofsd.sock --shared-dir /opt --cache auto &
 
 # bash -c "/usr/libexec/virtiofsd --socket-path=/tmp/virtiofsd.sock --shared-dir /opt --cache auto" &
 
