0f61005e3465def1bb4dbeb5ab7a5f1d9004218f
Fhwang <donghwa.dev@gmail.com>
2024-02-07 22:23:30 +0900

    test

--- src/entry.sh
+++ src/entry.sh
@@ -16,7 +16,7 @@ cd /run
 . proc.sh       # Initialize processor
 . power.sh      # Configure shutdown
 . config.sh     # Configure arguments
-bash custom.sh     # Configure custom
+. custom.sh     # Configure custom
 
 trap - ERR
 
