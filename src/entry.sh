f390d9625b78f0ddf99f8ff8c317650541a81476
Fhwang <donghwa.dev@gmail.com>
2024-02-08 00:40:52 +0900

    test

--- src/entry.sh
+++ src/entry.sh
@@ -8,15 +8,25 @@ SUPPORT="https://github.com/dockur/windows"
 cd /run
 
 . reset.sh      # Initialize system
+echo reset.sh init
 . install.sh    # Run installation
+echo install.sh init
 . disk.sh       # Initialize disks
+echo disk.sh init
 . display.sh    # Initialize graphics
+echo display.sh init
 . network.sh    # Initialize network
+echo network.sh init
 . boot.sh       # Configure boot
+echo boot.sh init
 . proc.sh       # Initialize processor
+echo proc.sh init
 . custom.sh     # Configure custom
+echo custom.sh init
 . power.sh      # Configure shutdown
+echo power.sh init
 . config.sh     # Configure arguments
+echo config.sh init
 
 trap - ERR
 
