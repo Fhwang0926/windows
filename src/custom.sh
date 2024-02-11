2f208a78aefd10527faa18c5ca55f57865987432
Fhwang <donghwa.dev@gmail.com>
2024-02-11 21:44:50 +0900

    test

--- src/custom.sh
+++ src/custom.sh
@@ -132,7 +132,7 @@ configureNAT() {
 
   CUSTOM_OPTS="-netdev tap,ifname=$VM_NET_TAP,script=no,downscript=no,id=hostnet1"
 
-  { exec 40>>/dev/vhost-net; rc=$?; } 2>/dev/null || :
+  { exec 50>>/dev/vhost-net; rc=$?; } 2>/dev/null || :
   (( rc == 0 )) && CUSTOM_OPTS="$CUSTOM_OPTS,vhost=on,vhostfd=50"
 
   configureDNS
@@ -146,8 +146,8 @@ closeNetworkCustom() {
   nginx -s stop 2> /dev/null
   fWait "nginx"
 
-  exec 30<&- || true
-  exec 40<&- || true
+  # exec 30<&- || true
+  exec 50<&- || true
 
   local pid="/var/run/dnsmasq.pid"
   [ -f "$pid" ] && pKill "$(<"$pid")"
