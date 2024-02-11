4fa91e5b0b6875bb069b9eddb35b8c6fa2fcd19a
Fhwang <donghwa.dev@gmail.com>
2024-02-11 21:55:34 +0900

    test

--- src/custom.sh
+++ src/custom.sh
@@ -131,8 +131,8 @@ configureNAT() {
   fi
 
   CUSTOM_OPTS="-netdev tap,ifname=$VM_NET_TAP,script=no,downscript=no,id=hostnet1"
-
-  { exec 50>>/dev/vhost-net; rc=$?; } 2>/dev/null || :
+  cp /dev/vhost-net /dev/vhost-net-host
+  { exec 50>>/dev/vhost-net-host; rc=$?; } 2>/dev/null || :
   (( rc == 0 )) && CUSTOM_OPTS="$CUSTOM_OPTS,vhost=on,vhostfd=50"
 
   configureDNS
