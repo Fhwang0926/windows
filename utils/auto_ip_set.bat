71a296c3cbd1f6877de1768ae83140304933920b
Fhwang <donghwa.dev@gmail.com>
2024-02-12 21:19:46 +0900

    test

--- utils/auto_ip_set.bat
+++ utils/auto_ip_set.bat
@@ -10,7 +10,7 @@ for /f "tokens=4" %%i in ('netsh interface show interface ^| findstr /R /C:"^.*E
 
 :setAddress
 IF NOT "!INTERFACE_NAME!"=="" (
-    netsh interface ip set address name="!INTERFACE_NAME!" static 10.20.0.30 255.255.255.0 10.20.0.1 1
+    netsh interface ip set address name="!INTERFACE_NAME!" static WIN_IP 255.255.255.0 WIN_GW 1
     netsh interface ip set dns name="!INTERFACE_NAME!" static 8.8.8.8
 
     echo renew !INTERFACE_NAME!
