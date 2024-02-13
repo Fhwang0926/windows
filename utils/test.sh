b2f78e9172826f085226d4d70de295479ea1ba37
Fhwang <donghwa.dev@gmail.com>
2024-02-13 21:16:55 +0900

    test

---
+++ utils/test.sh
@@ -0,0 +1,5 @@
+git pull && docker image build -t win:base .
+docker image tag win:base harbor.donghwa.dev:4443/seo/windows:2016
+docker image tag win:base harbor.donghwa.dev:4443/seo/windows:2022
+docker image tag win:base harbor.donghwa.dev:4443/seo/windows:win10
+docker image tag win:base harbor.donghwa.dev:4443/seo/windows:win11
