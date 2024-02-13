ea27b8da7db03f1dcca350d04ca9f940221b64b3
Fhwang <donghwa.dev@gmail.com>
2024-02-13 21:17:22 +0900

    test

--- utils/test.sh
+++ utils/test.sh
@@ -3,3 +3,4 @@ docker image tag win:base harbor.donghwa.dev:4443/seo/windows:2016
 docker image tag win:base harbor.donghwa.dev:4443/seo/windows:2022
 docker image tag win:base harbor.donghwa.dev:4443/seo/windows:win10
 docker image tag win:base harbor.donghwa.dev:4443/seo/windows:win11
+echo done!
