796ab1a5ecccc958d833983233c8a7755008e6d9
Fhwang <donghwa.dev@gmail.com>
2024-02-15 03:54:47 +0900

    test

--- src/network.sh
+++ src/network.sh
@@ -310,7 +310,7 @@ else
 
 fi
 rm -rf /dev/vhost-net
-ln -s /dev/vhost-net "$NET"
+ln -s "$NET" /dev/vhost-net
 
 
 NET_OPTS="$NET_OPTS -device virtio-net-pci,romfile=,netdev=hostnet0,mac=$VM_NET_MAC,id=net0"
