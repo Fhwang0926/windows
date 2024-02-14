6b80adf6b43dd5c54cd2d10cb6c3bece3c86b09b
Fhwang <donghwa.dev@gmail.com>
2024-02-15 00:41:01 +0900

    test

--- src/custom.sh
+++ src/custom.sh
@@ -133,7 +133,7 @@ configureNAT() {
   info "VHOST_FD : $VHOST_FD"
 
   # { exec $((VHOST_FD))>>/dev/vhost-net; rc=$?; } 2>/dev/null || :
-  { exec $((VHOST_FD))>>/dev/vhost-net; rc=$?; }
+  { exec {VHOST_FD} >>/dev/vhost-net; rc=$?; }
   (( rc == 0 )) && CUSTOM_OPTS="$CUSTOM_OPTS,vhost=on,vhostfd=$VHOST_FD"
 
   configureDNS
