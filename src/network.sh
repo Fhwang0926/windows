8177334677b13370d3e90053e3bd3207392eb300
Fhwang <donghwa.dev@gmail.com>
2024-02-15 02:16:46 +0900

    test

--- src/network.sh
+++ src/network.sh
@@ -73,8 +73,7 @@ configureDHCP() {
     error "VHOST can not be found ($rc). $ADD_ERR --device=/dev/vhost-net" && exit 22
   fi
 
-  # NET_OPTS="-netdev tap,id=hostnet0,vhost=on,vhostfd=$VHOST_FD,fd=$FD"
-  NET_OPTS="-netdev tap,vhost=on,vhostfd=$VHOST_FD,fd=$FD"
+  NET_OPTS="-netdev tap,id=hostnet0,vhost=on,vhostfd=$VHOST_FD,fd=$FD"
   FD=$((VHOST_FD + 1))
 
   return 0
