6dcada5f19970100a5c5642b371ed04ea410c2d7
Fhwang <donghwa.dev@gmail.com>
2024-03-02 02:12:10 +0900

    test

--- utils/auto_ip_set.bat
+++ utils/auto_ip_set.bat
@@ -6,25 +6,21 @@ set ipAddress=WIN_IP
 set subnetMask=WIN_SN
 set gateway=WIN_GW
 
-@REM Check if the adapter has a gateway set
 
-set gatewayConfigured=No
-@REM for /f "tokens=3" %%a in ('netsh interface ip show config name^="%adapterName%" ^| findstr /C:"게이트웨이" /C:"Default Gateway"') do (
-@REM     if not "%%a"=="" set gatewayConfigured=Yes
-@REM )
-
-@REM :: 게이트웨이가 설정되지 않았다면 IP 주소, 서브넷 마스크, 게이트웨이 설정
-if "%gatewayConfigured%"=="No" (
+ping -n 1 %gateway% >nul
+if %errorlevel%==0 (
+    echo Connection already.
+) else (
     net use Z: \\host.lan\common /persistent:yes
-
     echo Setting network configuration for %adapterName%...
+
     netsh interface ip set address name="%adapterName%" static %ipAddress% %subnetMask% %gateway%
     netsh interface ip set dns name="%adapterName%" static 8.8.8.8 primary
     netsh interface ip add dns name="%adapterName%" 8.8.8.8 index=2
+
     echo Network configuration has been set.
-) else (
-    echo %adapterName% already has a gateway configured. No changes made.
 )
+
 
 echo NIC configuration complete.
 
