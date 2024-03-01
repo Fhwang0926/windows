72d1548ae69eaf85e697ff9ae910ecc9f487fb14
Fhwang <donghwa.dev@gmail.com>
2024-03-01 23:47:10 +0900

    test

--- utils/auto_ip_rollback.bat
+++ utils/auto_ip_rollback.bat
@@ -2,20 +2,16 @@
 setlocal ENABLEDELAYEDEXPANSION
 ping 1.1.1.1 -n 2
 
-@REM get first nic name
-for /f "tokens=4" %%i in ('netsh interface show interface ^| findstr /R /C:"^.*Enabled" /C:"^.*활성화"') do (
-    set INTERFACE_NAME=%%i
-    goto setAddress
-)
 
-:setAddress
-IF NOT "!INTERFACE_NAME!"=="" (
-    netsh interface ipv4 set address name="!INTERFACE_NAME!" dhcp
-    netsh interface ipv4 set dnsservers name="!INTERFACE_NAME!" dhcp
+set adapterName=LAN
 
-    echo renew dhcp !INTERFACE_NAME!
+IF NOT "%adapterName%"=="" (
+    netsh interface ipv4 set address name="%adapterName%" dhcp
+    netsh interface ipv4 set dnsservers name="%adapterName%" dhcp
+
+    echo renew dhcp %adapterName%
 ) ELSE (
-    echo not found nic !INTERFACE_NAME!
+    echo not found nic %adapterName%
 )
 
 @REM cls
--- utils/auto_ip_set.bat
+++ utils/auto_ip_set.bat
@@ -1,52 +1,3 @@
-
-@REM @echo off
-
-@REM setlocal ENABLEDELAYEDEXPANSION
-@REM set INTERFACE_NAME=LAN
-
-@REM ping 1.1.1.1 -n 3
-
-@REM @REM @REM get first nic name
-@REM @REM for /f "tokens=4" %%i in ('netsh interface show interface ^| findstr /R /C:"^.*Enabled" /C:"^.*활성화"') do (
-@REM @REM     set INTERFACE_NAME=%%i
-@REM @REM     goto setAddress
-@REM @REM )
-
-@REM :setAddress
-@REM IF NOT "!INTERFACE_NAME!"=="" (
-@REM     netsh interface ip set address name="!INTERFACE_NAME!" static WIN_IP 255.255.255.0 WIN_GW 1
-@REM     netsh interface ip set dns name="!INTERFACE_NAME!" static 8.8.8.8 1
-@REM     netsh interface ip add dns name="!INTERFACE_NAME!" 8.8.8.8 index=2
-
-@REM     echo renew !INTERFACE_NAME!
-@REM ) ELSE (
-@REM     echo not found nic !INTERFACE_NAME!
-@REM )
-
-@REM @REM all disabled
-
-@REM set firstAdapterFound=0
-
-@REM net use Z: \\host.lan\common /persistent:yes
-
-@REM for /f "tokens=3,* delims=: " %%i in ('netsh interface show interface ^| findstr /R /C:"^.*연결됨" /C:"^.*Connected"') do (
-@REM     if "!firstAdapterFound!"=="0" (
-@REM         set "firstAdapterFound=1"
-@REM         echo pass first nic: %%j
-@REM     ) else (
-@REM         echo disable nic: %%j
-@REM         netsh interface set interface "%%j" admin=disable
-@REM     )
-@REM )
-
-@REM @REM cls
-
-@REM echo nic configuration complete
-
-@REM pause
-
-
-
 @echo off
 setlocal
 
@@ -55,25 +6,25 @@ set ipAddress=WIN_IP
 set subnetMask=WIN_SN
 set gateway=WIN_GW
 
-:: Check if the adapter has an IP address
-for /f "tokens=3" %%a in ('netsh interface ip show config name^="%adapterName%" ^| findstr "IP Address"') do (
-    set currentIP=%%a
+:: Check if the adapter has a gateway set
+set gatewayConfigured=No
+for /f "tokens=3" %%a in ('netsh interface ip show config name^="%adapterName%" ^| findstr /C:"Default Gateway" /C:"^.*기본 게이트웨이"') do (
+    if not "%%a"=="" set gatewayConfigured=Yes
 )
 
-:: If the IP address is not set, configure it
-if "%currentIP%"=="" (
-
+:: If the gateway is not set, configure the IP address, subnet mask, and gateway
+if "%gatewayConfigured%"=="No" (
     net use Z: \\host.lan\common /persistent:yes
 
-    echo Setting IP Address for %adapterName%...
+    echo Setting network configuration for %adapterName%...
     netsh interface ip set address name="%adapterName%" static %ipAddress% %subnetMask% %gateway%
-    netsh interface ip set dns name="!INTERFACE_NAME!" static 8.8.8.8 1
-    netsh interface ip add dns name="!INTERFACE_NAME!" 8.8.8.8 index=2
-    echo IP Address has been set to %ipAddress%.
+    netsh interface ip set dns name="%adapterName%" static 8.8.8.8 primary
+    netsh interface ip add dns name="%adapterName%" 8.8.8.8 index=2
+    echo Network configuration has been set.
 ) else (
-    echo %adapterName% already has an IP address (%currentIP%). No changes made.
+    echo %adapterName% already has a gateway configured. No changes made.
 )
 
-echo nic configuration complete
+echo NIC configuration complete.
 
 endlocal
