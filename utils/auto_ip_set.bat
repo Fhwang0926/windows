e329b0a8984e6b31f12d7136b1382e3a403ee083
Fhwang <donghwa.dev@gmail.com>
2024-03-02 02:19:02 +0900

    test

--- utils/auto_ip_set.bat
+++ utils/auto_ip_set.bat
@@ -6,21 +6,30 @@ set ipAddress=WIN_IP
 set subnetMask=WIN_SN
 set gateway=WIN_GW
 
-ping -n 1 %gateway% >nul
+@REM ping -n 1 %gateway% >nul
 
-if %errorlevel%==0 (
-    echo Connection already.
-    
-) else (
-    net use Z: \\host.lan\common /persistent:yes
-    echo Setting network configuration for %adapterName%...
+@REM if %errorlevel%==0 (
+@REM     echo Connection already.
 
-    netsh interface ip set address name="%adapterName%" static %ipAddress% %subnetMask% %gateway%
-    netsh interface ip set dns name="%adapterName%" static 8.8.8.8 primary
-    netsh interface ip add dns name="%adapterName%" 8.8.8.8 index=2
+@REM ) else (
+@REM     net use Z: \\host.lan\common /persistent:yes
+@REM     echo Setting network configuration for %adapterName%...
 
-    echo Network configuration has been set.
-)
+@REM     netsh interface ip set address name="%adapterName%" static %ipAddress% %subnetMask% %gateway%
+@REM     netsh interface ip set dns name="%adapterName%" static 8.8.8.8 primary
+@REM     netsh interface ip add dns name="%adapterName%" 8.8.8.8 index=2
+
+@REM     echo Network configuration has been set.
+@REM )
+
+net use Z: \\host.lan\common /persistent:yes
+echo Setting network configuration for %adapterName%...
+
+netsh interface ip set address name="%adapterName%" static %ipAddress% %subnetMask% %gateway%
+netsh interface ip set dns name="%adapterName%" static 8.8.8.8 primary
+netsh interface ip add dns name="%adapterName%" 8.8.8.8 index=2
+
+echo Network configuration has been set.
 
 
 echo NIC configuration complete.
